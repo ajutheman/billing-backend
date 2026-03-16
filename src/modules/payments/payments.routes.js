81ffc4cc61b1f0d9c265e3eb65665f2900de938c
ajmal.m <ajmal.m@saasvaap.com>
2026-03-17 01:41:15 +0530

    fix: initialize express.Router in modular route files

--- src/modules/payments/payments.routes.js
+++ src/modules/payments/payments.routes.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const router = express.Router();
 const { authenticateToken } = require('../../middleware/auth');
 const { dbRun, dbGet, dbAll } = require('../../core/db');
 
